c61d26236798470ca8249ea26fad963d5c2a4b1c
Mona Mohammed <mona00mona1997@gmail.com>
2023-09-16 22:37:20 +0300

    first Project Done

--- src/pages/WorkDetails2.jsx
+++ src/pages/WorkDetails2.jsx
@@ -3,12 +3,16 @@ import Markdown from "markdown-to-jsx";
 import fm from "front-matter";
 import Layout2 from "../components/Layout/Layout2";
 import { Link } from "react-router-dom/cjs/react-router-dom";
+import LoadingScreen from "../components/Loading/LoadingScreen";
 
 function WorkDetails2(props) {
   const [meta, setMeta] = useState("");
   const [content, setContent] = useState("");
   const title = props.match.params.title;
-
+  const [isloading, setIsLoading] = useState(true);
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 2000);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -32,28 +36,37 @@ function WorkDetails2(props) {
 
   return (
     <Layout2>
-      <section className="shadow-blue white-bg padding mt-0">
-        <ul className="list-inline portfolio-info mt-0">
-          <li className="list-inline-item">
-            <i className="icon-user"></i>
-            {meta.author}
-          </li>
-          <li className="list-inline-item">
-            <i className="icon-calendar"></i>
-            {meta.date}
-          </li>
-          <li className="list-inline-item">
-            <Link
-              to={meta && meta.link}
-              target="__blank"
-              rel="noopener noreferrer"
-            >
-              <i className="icon-link"></i> {meta && meta.link}
-            </Link>
-          </li>
-        </ul>
-        <Markdown children={content}></Markdown>
-      </section>
+      {isloading ? (
+        <LoadingScreen />
+      ) : (
+        <section className="shadow-blue white-bg padding mt-0">
+          <ul className="list-inline portfolio-info mt-0">
+            <li className="list-inline-item">
+              <i className="icon-user"></i>
+              {meta.author}
+            </li>
+            <li className="list-inline-item">
+              <i className="icon-calendar"></i>
+              {meta.date}
+            </li>
+            <li className="list-inline-item">
+              <Link
+                to={{ pathname: meta?.link }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="icon-link"></i> {meta && meta.title}
+              </Link>
+            </li>
+          </ul>
+          <Markdown children={content}></Markdown>
+          <ul className="list-unstyled d-flex">
+            {meta?.technologies?.map((el) => (
+              <li className="btn btn-default btn-lg mr-3 technologies">{el}</li>
+            ))}
+          </ul>
+        </section>
+      )}
     </Layout2>
   );
 }
---
+++ src/works/rssa.md
@@ -0,0 +1,28 @@
+---
+date: 6 September 2023
+author: Mona Mohammed
+link: https://rssa-monamohammed23.vercel.app/
+title : RSSA
+technologies : ['Bootstarp','jQuery','Slick Slider','Convert To Arabic']
+---
+
+### RSSA, Radiological Society of Saudi Arabia
+
+![RSSA](/images/rssa/profile.png)
+
+#### Overview
+
+A special website for the Saudi Scientific Society of Radiology,
+It contains the following pages:
+
+1- Homepage
+
+2 - About us
+
+3 - Media Center
+
+4 - Events
+
+5 - Contact Us
+
+A Website converted from a Figma design to a fully responsive app (perfect pixel) using these technologies: 
